cf797fa9b9ed7225f17e4cef1dc1659cac656f23
sefetekin23 <efe.tekin.23@ucl.ac.uk>
2023-11-14 02:38:28 +0000

    main last

--- main.c
+++ main.c
@@ -5,16 +5,15 @@
 
 int main(int argc, char const *argv[])
 {
-    int initialX = 3;
-    int initialY = 4;
+    int initialX = 7;
+    int initialY = 5;
     int initialDir = 1;
     int initialMarker = 0;
 
-    if (argc == 4) // Four arguments were typed
+    if (argc == 3) // Four arguments were typed
     {
         initialX = atoi(argv[1]);   // Get x value
         initialY = atoi(argv[2]);   // Get y value
-        initialDir = atoi(argv[3]); // Get direction
     }
     Robot robot;
     robot.x = initialX;
@@ -22,10 +21,11 @@ int main(int argc, char const *argv[])
     robot.dir = initialDir;
     robot.hasMarker = initialMarker;
     setGrid();
-
+    setMarkers();
+    setBlocks();
     // setting home
     grid[initialY][initialX] = HOME;
-
+    
     drawBackground();
     goToStart(&robot);
     explore(&robot);
